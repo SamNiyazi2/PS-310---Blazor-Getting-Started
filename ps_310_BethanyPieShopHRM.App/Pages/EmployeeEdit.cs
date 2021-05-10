3238cd18d203b5c846588c1b3da944f09837ce51
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2021-05-10 06:05:33 -0500

    M03-09 - Completed.

---
+++ ps_310_BethanyPieShopHRM.App/Pages/EmployeeEdit.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using ps_310_BethantysPieShopHRM.Shared;
+using ps_310_BethanyPieShopHRM.App.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// 05/10/2021 05:38 am - SSN - [20210510-0536] - [001] - M03-09 - Demo: Adding the add employee form
+
+namespace ps_310_BethanyPieShopHRM.App.Pages
+{
+    public partial class EmployeeEdit
+    {
+
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+
+        public Employee Employee { get; set; } = new Employee();
+
+
+        async protected override Task OnInitializedAsync()
+        {
+            if ( int.TryParse(EmployeeId, out int _employeeID))
+            {
+                Employee = await EmployeeDataService.GetEmployeeDetails(_employeeID);
+            }
+
+            // return base.OnInitializedAsync();
+        }
+    }
+}
